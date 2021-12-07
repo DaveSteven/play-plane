9e16166f8c51d275ad7705450d85fe2e21349b61
wei.han <wei.han@tingjiandan.com>
2021-12-07 18:31:49 +0800

    添加新接口

--- .history/src/runtime-canvas/index_20211207162235.js
+++ .history/src/runtime-canvas/index_20211207162235.js
@@ -1,7 +1,7 @@
 /*
  * @Author: David
  * @Date: 2021-12-06 14:45:39
- * @LastEditTime: 2021-12-07 18:31:19
+ * @LastEditTime: 2021-12-07 16:22:34
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /play_plane/src/runtime-canvas/index.js
@@ -12,7 +12,7 @@ import { Graphics, Text } from "pixi.js";
 const renderer = createRenderer({
     createElement(type) {
         let element;
-        if (type === 'rect') {
+       if (type === 'rect') {
             element = new Graphics();
             element.beginFill(0xff0000);
             element.drawRect(0, 0, 500, 500);
@@ -41,12 +41,7 @@ const renderer = createRenderer({
 
     insert(el, parent) {
         parent.addChild(el)
-    },
-
-    createComment() {},
-    parentNoder() {},
-    nextSibling() {},
-    remove(el) {}
+    }
 
 });
 
--- .history/src/runtime-canvas/index_20211207163020.js
+++ .history/src/runtime-canvas/index_20211207163020.js
@@ -1,7 +1,7 @@
 /*
  * @Author: David
  * @Date: 2021-12-06 14:45:39
- * @LastEditTime: 2021-12-07 18:31:19
+ * @LastEditTime: 2021-12-07 16:30:19
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /play_plane/src/runtime-canvas/index.js
@@ -41,12 +41,7 @@ const renderer = createRenderer({
 
     insert(el, parent) {
         parent.addChild(el)
-    },
-
-    createComment() {},
-    parentNoder() {},
-    nextSibling() {},
-    remove(el) {}
+    }
 
 });
 
